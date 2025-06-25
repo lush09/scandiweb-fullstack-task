76a3ee10b1a1e5cc1615de526c229ff64a9a4af1
lush09 <87174432+lush09@users.noreply.github.com>
2025-06-25 15:20:56 +0800

    deploym1

--- scandiweb-frontend/src/apollo/client.ts
+++ scandiweb-frontend/src/apollo/client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:8000/graphql", // Adjust to backend URL
+  uri: "https://scandiweb-backend.42web.io/graphql", // Production backend URL
   cache: new InMemoryCache(),
 });
 
